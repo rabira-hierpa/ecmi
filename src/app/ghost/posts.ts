d9707db15c28311a7f712a732705c5f309a1d57b
rabra <rabira.hierpa@gmail.com>
2024-10-08 15:30:45 +0300

    refactor: Update Ghost API configuration and usage
    This commit updates the Ghost API configuration and usage in the codebase. The `GhostContentAPI` import has been replaced with the `api` import from the local `api` module. This change ensures consistency and improves maintainability.
    
    Changes:
    - Removed the `GhostContentAPI` import from `src/app/ghost/posts.ts` and `src/app/ghost/settings.ts`
    - Imported `api` from `./api` in `src/app/ghost/posts.ts` and `src/app/ghost/settings.ts`
    
    Refactoring the code in this way allows for easier modification and extension of the API configuration in the future.

--- src/app/ghost/posts.ts
+++ src/app/ghost/posts.ts
@@ -1,11 +1,5 @@
-import GhostContentAPI from "@tryghost/content-api";
+import api from "./api";
 // Create API instance with site credentials
-
-const api = new GhostContentAPI({
-  url: String(process.env.NEXT_PUBLIC_GHOST_URL).replace("/ghost", ""),
-  key: process.env.NEXT_PUBLIC_GHOST_CONTENT_API_KEY as string,
-  version: "v4.8",
-});
 
 export async function getPosts() {
   return await api.posts
--- src/app/ghost/settings.ts
+++ src/app/ghost/settings.ts
@@ -1,12 +1,4 @@
-import GhostContentAPI from "@tryghost/content-api";
-// Create API instance with site credentials
-
-const api = new GhostContentAPI({
-  url: String(process.env.NEXT_PUBLIC_GHOST_URL).replace("/ghost", ""),
-  key: process.env.NEXT_PUBLIC_GHOST_CONTENT_API_KEY as string,
-  version: "v4.8",
-});
-
+import api from "./api";
 export async function getNavbar() {
   const settings = await api.settings.browse();
   return settings.navigation;
